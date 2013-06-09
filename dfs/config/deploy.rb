fa5bb64299724a1c15fe24e0f5b5153680580515
msimonin <matthieu.simonin@inria.fr>
2013-06-09 20:02:55 +0200

    move to bundler move to deployed nodes ..
    M

--- dfs/config/deploy.rb
+++ dfs/config/deploy.rb
@@ -1,9 +1,9 @@
 set :g5k_user, "msimonin"
 set :gateway, "#{g5k_user}@access.grid5000.fr"
 # ssh key for the gateway
-ssh_options[:keys]= [File.join(ENV["HOME"], ".ssh_cap", "id_rsa_cap")]
+ssh_options[:keys]= [File.join(ENV["HOME"], ".ssh_grid5000", "id_rsa")]
 # this key will be copied to deployed nodes
-set :ssh_public,  File.join(ENV["HOME"], ".ssh_insideg5k", "id_rsa.pub")
+set :ssh_public,  File.join(ENV["HOME"], ".ssh", "id_rsa_insideg5k.pub")
 
 set :walltime, ENV['walltime'] || XP5K::Config[:walltime] || "1:00:00"
 set :subnet, ENV['subnet'] || "slash_22"
